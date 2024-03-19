2cfc5c2c1ce324895115314e0e31fc9d100a8519
seoilgun <seoilgun@gmail.com>
2024-03-19 12:32:38 +0900

    d

--- inpaint_train.py
+++ inpaint_train.py
@@ -5,7 +5,7 @@ from torch.utils.data import DataLoader
 from inpaint_dataset import InpaintDataset
 from cldm.logger import ImageLogger
 from cldm.model import create_model, load_state_dict
-from lightning.pytorch.callbacks import ModelCheckpoint
+from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
 import argparse
 
 
