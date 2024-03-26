7b48a9e2ef91752d2c7e83c824482b11cbbb6495
seoilgun <seoilgun@gmail.com>
2024-03-26 12:00:21 +0900

    d

--- extract_only_weights_from_model.py
+++ extract_only_weights_from_model.py
@@ -7,6 +7,7 @@ from cldm.model import create_model, load_state_dict
 def main(args):
     model = create_model(args.model_config).cpu()
     model.load_state_dict(load_state_dict(args.model_path, location='cpu'))
+    os.makedirs(os.path.dirname(args.output_path), exist_ok=True)
     torch.save(model.state_dict(), args.output_path)
 
     print(f'Weights saved to [{args.output_path}]')
